69266f2039d1af854d2a645bc5da4f94446dd963
Taylor <tarkington.taylor@gmail.com>
2018-07-24 19:23:58 -0500

    update

---
+++ Final Project/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    document.getElementById('subscribeBtn').addEventListener('click', submit);
+
+    function submit(){
+
+        alert('You clicked the subscribe button!');
+    };
+
+   
+});
